9672df904f7b93927c6c5c690a916d5c62472e80
Jawann <jawann@gmail.com>
2017-12-06 02:02:50 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,10 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(file)
     @path = file
+    @files = Dir.entries(file)
+    @files.shift
+    @files.shift
   end
 end
