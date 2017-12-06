dcdf970426c84f070cc07e95b1da6f8cad252606
Jawann <jawann@gmail.com>
2017-12-06 02:44:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(file)
     artist, song, genre = file.split(' - ')
     temp = Song.new(song)
-    temp.artist = artist
+    temp.artist = Artist.new(artist)
     temp
   end
 end
